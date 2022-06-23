07d2a5c7417573d82f5e5777dbd9f800693c53ab
Azat Davliatshin <ardavlyatshin@gmail.com>
2022-06-23 22:07:19 +0300

    Create task-2.2.py

---
+++ module-2/task-2.2.py
@@ -0,0 +1,17 @@
+# Task 2.2
+#Write a Python program to count the number of characters (character frequency) in a string (ignore case of letters).
+#Examples:
+
+# Input: 'Oh, it is python' 
+# Output: {',': 1, ' ': 3, 'o': 2, 'h': 2, 'i': 2, 't': 2, 's': 1, 'p': 1, 'y': 1, 'n': 1}
+
+def calculate_characters_frequency(str):
+    char_freq_map = {}
+    str = str.lower()
+
+    for char in str: 
+        char_freq_map[char] = str.count(char)
+    return char_freq_map
+
+expected_result = {',': 1, ' ': 3, 'o': 2, 'h': 2, 'i': 2, 't': 2, 's': 1, 'p': 1, 'y': 1, 'n': 1}
+assert calculate_characters_frequency('Oh, it is python') == expected_result
