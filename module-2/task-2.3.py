af02fde30d520df8826577a564007880db134095
Azat Davliatshin <ardavlyatshin@gmail.com>
2022-06-23 22:24:26 +0300

    Create task-2.3.py

---
+++ module-2/task-2.3.py
@@ -0,0 +1,18 @@
+# Task 2.3
+# Write a Python program that accepts a comma separated sequence of words as input and prints the unique words in sorted form.
+# Examples:
+
+# Input: ['red', 'white', 'black', 'red', 'green', 'black']
+# Output: ['black', 'green', 'red', 'white']
+
+def normalize_list(list, reverse=False):
+    return sorted(set(list), reverse=reverse);
+
+expected_result = ['black', 'green', 'red', 'white']
+assert normalize_list(['red', 'white', 'black', 'red', 'green', 'black']) == expected_result
+
+expected_result.reverse();
+assert normalize_list(['red', 'white', 'black', 'red', 'green', 'black'], True) == expected_result
+
+assert normalize_list([1,1,9,5,3,7,2,2,8,6,3,4,2]) == list(range(1, 10))
+
