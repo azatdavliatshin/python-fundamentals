c0db76dba3af264a23fa9b1f189711d9ed519493
Azat Davliatshin <ardavlyatshin@gmail.com>
2022-08-03 22:47:48 +0300

    Task 4_2 complete

---
+++ functions/task4_2.py
@@ -0,0 +1,20 @@
+### Task 4.2
+# Write a function that check whether a string is a palindrome or not. Usage of
+# any reversing functions is prohibited. To check your implementation you can use
+# strings from [here](https://en.wikipedia.org/wiki/Palindrome#Famous_palindromes).
+
+def isStringPalindrome(str):
+    i = 0
+    j = len(str) - 1
+
+    while (i < j):
+        if str[i].lower() != str[j].lower():
+            return False
+        i += 1
+        j -= 1
+    return True;
+
+assert isStringPalindrome("Test") == False
+assert isStringPalindrome("racecar") == True
+assert isStringPalindrome("radar") == True
+
